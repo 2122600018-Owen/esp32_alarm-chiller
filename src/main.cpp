0f76da17d583a1d9ff5086605ec3e3f892564f4f
Mine292 <owensanjaya@gmail.com>
2025-05-08 10:52:04 +0700

    modified:   platformio.ini     > Added 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <SPI.h>
-#include <max6675.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_GFX.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 // put pin declarations here:
-int thermoDO = 19;
-int thermoCS = 23;
-int thermoCLK = 5;
 int relay = 32;
+int oneWireBus = 5;
 
 // put value variables here:
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -24,8 +22,9 @@ float minTemp =25.0;
 bool heatWarning = false;
 
 // put object declarations here:
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+OneWire oneWire(oneWireBus);
+DallasTemperature sensors(&oneWire);
 
 // put function declarations here:
 
@@ -34,17 +33,16 @@ void setup() {
   // Begin Serial
   Serial.begin(115200);
   Serial.println("Serial Started...");
-
+  
+  // Begin oneWire sensors
+  sensors.begin();
+  
+  // Begin SSD1306 OLED display
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  //display.display();
-  //delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
   display.clearDisplay();
@@ -57,7 +55,6 @@ void setup() {
   display.display();
   delay(2000); // Pause for 2 seconds
   
-
   pinMode(relay, OUTPUT);
   digitalWrite(relay, LOW);
 }
@@ -66,37 +63,27 @@ void loop() {
   // clear the display for next loop
   display.clearDisplay();
 
-  // basic readout test, just print the current temp on serial monitor
-  Serial.print("C = ");
-  Serial.println(thermocouple.readCelsius());
+  // basic readout serial monitor print from DS18B20
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
 
   // display current temperature and thresholds
-  //display.setTextSize(1);      // Normal 1:1 pixel scale
   display.setTextSize(2);      // Larger 2:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(0,8);
-  //display.print("Temp: ");
-  display.print(thermocouple.readCelsius());
+  display.print(temperatureC);
   display.print((char)247);display.println("C");
-  //display.print("Max : ");
-  //display.print(maxTemp);
-  //display.println(" C");
-  //display.print("Min : ");
-  //display.print(minTemp);
-  //display.println(" C");
   display.display();
 
   
   // temperature threshold checking
-  if (thermocouple.readCelsius() >= maxTemp) {
+  if(temperatureC >= maxTemp){
     digitalWrite(relay, HIGH);
     heatWarning = 1;
   }
-  //else if(thermocouple.readCelsius() <= minTemp){
-  //  digitalWrite(relay, LOW);
-  //  //heatWarning = 0;
-  //  Serial.println("LO-TEMP");
-  //}
+
   if(heatWarning){
     display.setTextSize(1);      // Normal 1:1 pixel scale
     display.setCursor(86,8);
@@ -109,9 +96,6 @@ void loop() {
     display.print("|ALERT|");
     Serial.println("HI-TEMP");
     display.display();
-    //digitalWrite(relay, HIGH);
-    //delay(1000);
-    //digitalWrite(relay, LOW);
   }
 
   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
